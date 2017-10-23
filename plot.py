7c96ab8c1bf0343d80ee659f7825248a0ea9b261
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-10-23 10:43:28 +0900

    Created empty plot module

---
+++ plot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module creates plots for the report using matplotlib.
+"""
+from matplotlib import pyplot
+
+def create_plots(plot_data):
+    """
+    Create plots based on the given plot_data.
+    """
