d36e6663d21b594e75587d59ce6c407af2271130
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-10-23 10:15:55 +0900

    Created a report module for generating reports from a template

---
+++ report.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module creates a HTML report based on the data from the parsed file.
+"""
+import os
+from jinja2 import Environment, FileSystemLoader
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.join(PATH, 'templates')),
+    trim_blocks=False)
+
+def _render_template(template_filename, data):
+    return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(data)
+
+def create_report(report_data):
+    """
+    Creates a report.html file in report/ directory using the template and
+    given data.
+    """
+    output_file = 'report/report.html'
+    with open(output_file, 'w') as output:
+        html = _render_template('template.html', report_data)
+        output.write(html)
