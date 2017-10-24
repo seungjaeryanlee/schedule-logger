5ec0c88085f5a9461e1ce3e969d36648685ad5f9
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-10-24 13:46:58 +0900

    Issue 59: Fixed bug in computing total time for classifications (#62)
    * Fixed bug where 12 hours are added to delta_time, not this_time after ~ token
    
    * Fixed bug where neither_time is not added delta_time but assigned value of delta_time

--- parse.py
+++ parse.py
@@ -226,13 +226,13 @@ def parse_file(filename):
         time_str = line[0:5].strip()
         this_time = _get_timedelta_from_string(time_str)
 
+        # If after 12:59, add 12 hours to make it 24-hour format
+        if is_pm:
+            this_time += timedelta(hours=12)
+
         # Calculate timedelta
         delta_time = this_time - previous_time
         previous_time = this_time
-
-        # If after 12:59, add 12 hours to make it 24-hour format
-        if is_pm:
-            delta_time += timedelta(hours=12)
 
         # Parse actions
         tokens = line[5:].split('/')
@@ -260,7 +260,7 @@ def parse_file(filename):
                     _update_dict(worthy_dict, line[5:], delta_time)
                     break
                 elif answer == 'N':
-                    neither_time = delta_time
+                    neither_time += delta_time
                     _update_dict(neither_dict, line[5:], delta_time)
                     break
                 elif answer == 'R':
