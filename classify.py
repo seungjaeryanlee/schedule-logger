efc7233eb500e00e2d4d67d42dbc78579d06b076
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-05-08 09:59:25 +0900

    Added empty classify_action()

--- classify.py
+++ classify.py
@@ -2,3 +2,9 @@
 This module classifies an action as Worthy, Rest, or Neither.
 """
 
+def classify_action(action):
+    """
+    Returns 'W', 'R', or 'N' if the given action is Worthy, Rest, or Neither.
+    """
+
+    return 'N'
