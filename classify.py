e873eb7143a77cc90507f2983cf3decc3187357b
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-05-08 10:20:09 +0900

    Implemented import_regex_to_class_dict()

--- classify.py
+++ classify.py
@@ -1,10 +1,24 @@
 """
 This module classifies an action as Worthy, Rest, or Neither.
 """
+import re
+
+def import_regex_to_class_dict():
+    """
+    Imports regex-to-class dictionary from regex_to_class.csv.
+    """
+    regex_to_class = {}
+    with open('regex_to_class.csv') as file:
+        lines = file.readlines()
+        for line in lines:
+            regex, classification = line.strip().split(',')
+            regex_to_class[regex] = classification
+
+    return regex_to_class
 
 def import_action_to_class_dict():
     """
-    Imports action-to-class dictionary from action_to_class.csv
+    Imports action-to-class dictionary from action_to_class.csv.
     """
     action_to_class = {}
     with open('action_to_class.csv') as file:
@@ -13,13 +27,24 @@ def import_action_to_class_dict():
             action, classification = line.strip().split(',')
             action_to_class[action] = classification
 
-    print(action_to_class)
     return action_to_class
 
 
-def classify_action(action_to_class, action):
+def classify_action(regex_to_class, action_to_class, action):
     """
     Returns 'W', 'R', or 'N' if the given action is Worthy, Rest, or Neither.
     """
+    for regex, classification in regex_to_class.items():
+        if re.search(regex, action):
+            return classification
 
-    return 'N'
+    if action_to_class[action]:
+        return action_to_class[action]
+
+    return None
+
+if __name__ == '__main__':
+    atoc = import_action_to_class_dict()
+    rtoc = import_regex_to_class_dict()
+    c = classify_action(rtoc, atoc, 'Rest (asdf)')
+    print(c)
